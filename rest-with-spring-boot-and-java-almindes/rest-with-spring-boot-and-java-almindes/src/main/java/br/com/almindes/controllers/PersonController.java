62c4344c8b618141cade2c2b6a249a47d6907c26
Rafael Almindes <rafaalmindes@gmail.com>
2025-02-24 10:22:42 -0300

    Retornando Listas de Objetos em JSON

--- rest-with-spring-boot-and-java-almindes/rest-with-spring-boot-and-java-almindes/src/main/java/br/com/almindes/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-almindes/rest-with-spring-boot-and-java-almindes/src/main/java/br/com/almindes/controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -17,6 +19,12 @@ public class PersonController {
     private PersonServices service;
     // private PersonService service = new PersonService();
 
+    @RequestMapping(method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
     @RequestMapping(value = "/{id}",
         method = RequestMethod.GET,
         produces = MediaType.APPLICATION_JSON_VALUE
